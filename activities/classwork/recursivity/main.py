53803a7f19b523e0ef748cb173f5edbcedc824bc
ferpinedot <mfernandapinedot@hotmail.com>
2019-09-18 19:16:51 -0500

    Create  recursive implementation

--- activities/classwork/recursivity/main.py
+++ activities/classwork/recursivity/main.py
@@ -1,25 +1,44 @@
+#Libraries Importation
 import fire
 import logging
+from util import TriangleBuilder, caching, timeit
 
-from util import TriangleBuilder
-
+logger = logging.getLogger(__name__)
 
 class Main(object):
+    builder=TriangleBuilder() #Define a function on a variable
 
+    #staticmethod
     def message(self, content):
         logging.info(content)
 
+    @caching #decorator
     def get_element_recursively(self, i, j):
-        pass
+        return 1 if (j >= i or j == 0) else \
+            self.get_element_recursively(i=i - 1, j=j) + \
+            self.get_element_recursively(
+            i=i - 1, j=j - 1)
 
     def pascal_triangle_a(self, level, index=0):
-        pass
+        if index < level:
+            row = [str(self.get_element_recursively(i=index, j=j)) for j in range(index + 1)]
+            print(" ".join(row))
+            self.pascal_triangle_a(level=level, index=index + 1)
 
     def pascal_triangle_b(self, level, index=0):
-        pass
+        if index < level:
+            row = self.builder.get_row(index=index)
+            print(" ".join(row))
+            self.pascal_triangle_b(level, index=index+1)
 
+    @timeit(logger)
     def pascal_triangle(self, level, option):
-        pass
+        if "a" == option.lower():
+            self.pascal_triangle_a(level=level, index=0)
+        elif "b" == option.lower():
+            self.pascal_triangle_b(level=level, index=0)
+        else:
+            logger.info(f"Option {option}  undefined.") #.format(option=option))
 
 
 if __name__ == "__main__":
