3051376e6402db82acf35d96cbeba05c6e9ffd7f
ferpinedot <mfernandapinedot@hotmail.com>
2019-10-25 20:07:49 -0500

    classwork

--- activities/classwork/enrollment-database/exercise.sql
+++ activities/classwork/enrollment-database/exercise.sql
@@ -5,14 +5,65 @@
 
 
 -- A) Get all the unique department names.
+-- * es seleccionar todas las columnas
+SELECT DISTINCT department FROM  course;
+-- DISTINCT seleccionar los valores únicos de department
+
 
 -- B) Get the top 10 female students (first_name, last_name, age, gpa) with the best GPA scores and order by age (asc).
+SELECT
+	first_name AS "first name",
+	last_name AS "Last name",
+	age AS "Age",
+	gpa AS "GPA"
+FROM
+	student
+WHERE
+	gender = 'female' -- aquí se podrían poner diferentes condiciones
+ORDER BY
+	gpa DESC, -- DESC = Descendiente
+	age ASC,
+	first_name ASC,
+	last_name ASC
+LIMIT 10
+;
+
 
 -- C) Count the number of male/female students that are at least 25 years old.
+SELECT
+	gender AS "Gender",
+	count(gender) AS "Count"
+FROM
+	student
+WHERE
+	age >= 25
+GROUP BY gender
+;
+
+
 
 -- D) Get the number of male/female students that were accepted
+SELECT
+    gender,
+    count(gender)
+FROM
+	student INNER JOIN enrollment ON student.id = enrollment.student_id
+WHERE
+    enrollment.approved = 1
+GROUP BY
+    gender
+;
 
 -- E) Get the min, average, and max GPA of the accepted male students that are less than 20 years old.
+SELECT
+    MIN(gpa),
+    AVG(gpa),
+    MAX(gpa)
+FROM
+	student INNER JOIN enrollment ON student.id = enrollment.student_id
+WHERE
+    enrollment.approved = 1
+GROUP BY
 
 -- F) Get the number of enrollments to courses that take longer than 2 years to finalize.
 
