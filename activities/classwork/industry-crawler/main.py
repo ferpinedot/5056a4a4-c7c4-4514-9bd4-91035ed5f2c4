b88a8deceafeb1b0996e878a630bee61ccb73db8
ferpinedot <mfernandapinedot@hotmail.com>
2019-10-26 18:10:48 -0500

    Tarea Industry crawler

--- activities/classwork/industry-crawler/main.py
+++ activities/classwork/industry-crawler/main.py
@@ -14,8 +14,18 @@ logger = logging.getLogger(__name__)  # logger es como print pero tiene más fun
 
 class Main(object):
 
-    def _recursive_search(self, node, string_wrapper, exact): #cambiar o modificar argumentos
-        pass  #IMPLEMENTACIÓN DE FUNCIÓN
+    def _recursive_search(self, node, string_wrapper, exact):
+        title = node["title"]
+        children = node["children"]
+        new_children = []
+        for child in children:
+            is_child_valid = self._recursive_search(child, string_wrapper, exact=exact)
+            if is_child_valid:
+                new_children.append(child)
+        node["children"] = new_children
+        succesful_search = len(new_children) or string_wrapper.boolean_search(title, reverse=True, exact=exact)
+        return (succesful_search)
+
 
     @staticmethod
     @timeit(logger)
@@ -30,7 +40,12 @@ class Main(object):
     def search(self, title, exact=False, filename=DEFAULT_INDUSTRY_FILE):
         target_title = StringWrapper(value=title)
         sic_industries = SIC.load_json(filename)
-        return []
+        children = sic_industries["children"]
+        new_children = []
+        for child in children:
+            if self._recursive_search(child, target_title, exact=exact):
+                new_children.append(child)
+        return new_children
 
 
 
